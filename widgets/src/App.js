7ab4626c758385d2f57fbf2e8e985d55f1af2b42
Harish Lalwani <harishlalwani2289@gmail.com>
2021-12-03 18:43:41 +0530

    Added navigation option.. commented code inside App.js is the non-react approach.

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -1,6 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import Accordion from "./components/Accordion";
 import Search from "./components/Search";
+import Dropdown from "./components/Dropdown";
+import Translate from "./components/Translate";
+import Route from "./route";
+import Header from "./components/Header";
 
 const items = [
   {
@@ -17,11 +21,80 @@ const items = [
   },
 ];
 
+const options = [
+  {
+    label: "The Color Red",
+    value: "red",
+  },
+  {
+    label: "Shade of Blue",
+    value: "blue",
+  },
+  {
+    label: "Misty Green",
+    value: "green",
+  },
+];
+
+// const showAccordion = () => {
+//   if (window.location.pathname === "/") {
+//     return <Accordion items={items} />;
+//   }
+// };
+
+// const showList = () => {
+//   if (window.location.pathname === "/list") {
+//     return <Search />;
+//   }
+// };
+
+// const showDropdown = () => {
+//   if (window.location.pathname === "/dropdown") {
+//     return <Dropdown />;
+//   }
+// };
+
+// const showTranslate = () => {
+//   if (window.location.pathname === "/translate") {
+//     return <Translate />;
+//   }
+// };
+
 export default () => {
+  const [selected, setSelected] = useState(options[0]);
   return (
     <div>
-      {/* <Accordion items={items} /> */}
-      <Search />
+      <Header />
+      {/* <Dropdown
+        options={options}
+        selected={selected}
+        onSelectedChange={setSelected}
+      /> */}
+      {/* {showAccordion()}
+      {showDropdown()}
+      {showList()}
+      {showTranslate()} */}
+
+      <Route path="/">
+        <Accordion items={items} />
+      </Route>
+
+      <Route path="/list">
+        <Search />
+      </Route>
+
+      <Route path="/dropdown">
+        <Dropdown
+          label="Select a Color"
+          options={options}
+          selected={selected}
+          onSelectedChange={setSelected}
+        />
+      </Route>
+
+      <Route path="/translate">
+        <Translate />
+      </Route>
     </div>
   );
 };
